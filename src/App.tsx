49fd2fac92cd96a723f03019a55abc61a82ba8a0
David <davidagamez04@gmail.com>
2024-09-13 09:26:17 -0500

    add: shadcn/ui alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Terminal } from 'lucide-react';
 
 function App() {
 
   return (<main>
-    <h1 className="underline">
-      Hello world!
-    </h1>
+    <Alert variant={ 'destructive' }>
+      <Terminal className="h-4 w-4"/>
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components and dependencies to your app using the cli.
+      </AlertDescription>
+    </Alert>
+
   </main>);
 }
 
