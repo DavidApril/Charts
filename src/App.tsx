80844cf2f0a380279a3c6b8375ca65348a13168b
David <davidagamez04@gmail.com>
2024-09-19 17:02:13 -0500

    feat: area chart implement on main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import {Navbar} from '@/components/navigation/navigation.tsx';
 import {ThemeProvider} from '@/provider/theme.tsx';
+import {ChartArea} from '@/components/charts/area.tsx';
 
 function App() {
+
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <main className={'w-4/5 mx-auto'}>
+      <main className={'w-4/5 mx-auto flex flex-col gap-3 my-3'}>
         <Navbar/>
+        <ChartArea/>
       </main>
     </ThemeProvider>
   );
