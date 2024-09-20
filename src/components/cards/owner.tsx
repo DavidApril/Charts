962434bbabc2f2ca926f555715935493e6550b1d
David <davidagamez04@gmail.com>
2024-09-19 21:07:01 -0500

    feat: owner card info replaced

--- src/components/cards/owner.tsx
+++ src/components/cards/owner.tsx
@@ -7,7 +7,8 @@ import {
   CardHeader,
   CardTitle,
   AvatarFallback,
-  ChartPie
+  ChartPie,
+  SocialNetworkBadgeWithTooltip
 } from '@/components';
 
 export const OwnerCard = () => {
@@ -15,13 +16,26 @@ export const OwnerCard = () => {
     <Card>
       <CardHeader>
         <div className={'flex gap-5'}>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png"/>
-            <AvatarFallback>CN</AvatarFallback>
+          <Avatar className={'h-60 w-60'}>
+            <AvatarImage src="https://github.com/DavidApril.png" alt="@DavidApril"/>
+            <AvatarFallback>D</AvatarFallback>
           </Avatar>
-          <div>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
+          <div className={'flex flex-col gap-2'}>
+            <CardTitle>Jesús David Agámez Cantillo</CardTitle>
+            <CardDescription>Front End Developer</CardDescription>
+            <CardDescription className={'text-xl'}>
+              Software Engineering student with more than a year and a half of experience as a front-end developer. My
+              ability to face new challenges and learn in an agile way allows me to successfully integrate into new
+              projects, quickly adapting to a new team and its methodology.
+            </CardDescription>
+            <CardDescription className={'flex flex-wrap gap-1'}>
+              <SocialNetworkBadgeWithTooltip
+                tooltipContent={'Go to Github'} label={'@DavidApril'}
+                to={'https://github.com/DavidApril'}/>
+              <SocialNetworkBadgeWithTooltip
+                tooltipContent={'Go to linkedin'} label={'LinkedIn'}
+                to={'https://www.linkedin.com/in/david-ag%C3%A1mez/'}/>
+            </CardDescription>
           </div>
         </div>
       </CardHeader>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,12 +1,19 @@
+export * from './badges/social-network.tsx';
+
 export * from './cards/owner.tsx';
 export * from './cards/profile-spotify.tsx';
+
 export * from './carousel/repositories.tsx';
 export * from './carousel/top-songs.tsx';
+
 export * from './charts/area.tsx';
 export * from './charts/pie.tsx';
+
 export * from './navigation/navigation.tsx';
+
 export * from './ui/alert.tsx';
 export * from './ui/avatar.tsx';
+export * from './ui/badge.tsx';
 export * from './ui/button.tsx';
 export * from './ui/card.tsx';
 export * from './ui/carousel.tsx';
@@ -16,3 +23,4 @@ export * from './ui/mode-toggle.tsx';
 export * from './ui/navigation-menu.tsx';
 export * from './ui/select.tsx';
 export * from './ui/toggle.tsx';
+export * from './ui/tooltip.tsx';
