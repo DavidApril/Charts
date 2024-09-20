6ebfb8c84ac5deff9050c061498effdeb48c6241
David <davidagamez04@gmail.com>
2024-09-20 00:33:28 -0500

    feat: get yizuskraist's spotify profile data

--- src/components/cards/profile-spotify.tsx
+++ src/components/cards/profile-spotify.tsx
@@ -7,23 +7,50 @@ import {
   CardHeader,
   CardTitle,
   AvatarFallback,
-  TopSongsCarousel
+  TopSongsCarousel,
+  SocialNetworkBadgeWithTooltip,
+  AvatarSkeleton,
+  Badge
 } from '@/components';
+import {SpotifyUserInfo} from '@/interfaces';
+import {SpotifyService} from '@/services';
+import {useEffect, useCallback, useState} from 'react';
 
 export const ProfileSpotifyCard = () => {
+
+  const [userProfile, setUserProfile] = useState<SpotifyUserInfo>();
+
+  const getProfileInformation = useCallback(() => {
+    SpotifyService.getProfileByUser('yizuskraist')
+      .then(u => setUserProfile(u));
+  }, []);
+
+  useEffect(() => {
+    getProfileInformation();
+  }, []);
+
+  console.log({userProfile});
+
   return (
     <Card>
       <CardHeader>
-        <div className={'flex gap-5'}>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png"/>
+        {userProfile ? <div className={'flex gap-5'}>
+          <Avatar className={'h-20 w-20'}>
+            <AvatarImage src={userProfile.images[0].url}/>
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <div>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
+          <div className={'flex flex-col gap-3 justify-center'}>
+            <CardTitle>{userProfile?.display_name}</CardTitle>
+            <CardDescription className={'flex gap-1'}>
+              <SocialNetworkBadgeWithTooltip
+                tooltipContent={`Go to ${userProfile?.display_name}'s Spotify profile`}
+                label={userProfile.id} to={userProfile.external_urls.spotify}/>
+              <Badge variant={'outline'}>
+                {userProfile.followers.total} followers
+              </Badge>
+            </CardDescription>
           </div>
-        </div>
+        </div> : <AvatarSkeleton/>}
       </CardHeader>
       <CardContent className={'flex place-content-center'}>
         <TopSongsCarousel/>
