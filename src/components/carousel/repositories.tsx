7da14abae98dd5f6eaff266648910af499b25058
David <davidagamez04@gmail.com>
2024-09-19 20:20:18 -0500

    style: repositories carousel re-styled

--- src/components/carousel/repositories.tsx
+++ src/components/carousel/repositories.tsx
@@ -1,5 +1,5 @@
 import {Card, CardContent} from '@/components/ui/card';
-import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from '@/components/ui/carousel';
+import {Carousel, CarouselContent, CarouselItem} from '@/components/ui/carousel';
 
 export const RepositoriesCarousel = () => {
   return (
@@ -7,23 +7,21 @@ export const RepositoriesCarousel = () => {
       opts={{
         align: 'center'
       }}
-      className="w-full max-w-sm"
+      className="w-full"
     >
-      <CarouselContent>
-        {Array.from({length: 5}).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+      <CarouselContent className={'flex-1'}>
+        {Array.from({length: 10}).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/6">
             <div className="p-1">
               <Card>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
+                  <span className="text-3xl font-semibold">R3p0 {index + 1}</span>
                 </CardContent>
               </Card>
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious/>
-      <CarouselNext/>
     </Carousel>
   );
 };
