1f3ddec92c0733844bda1956fe67f21de1f4b938
David <davidagamez04@gmail.com>
2024-09-20 23:21:41 -0500

    update: user card layout & content

--- src/components/cards/owner.tsx
+++ src/components/cards/owner.tsx
@@ -1,48 +1,52 @@
 import {
-  Avatar,
-  AvatarImage,
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
+  SocialNetworkBadgeWithTooltip,
+  Avatar,
+  AvatarImage,
   AvatarFallback,
-  ChartPie,
-  SocialNetworkBadgeWithTooltip
+  AvatarSkeleton,
 } from '@/components';
+import {GithubUser} from '@/interfaces';
+import {HTMLAttributes} from 'react';
 
-export const OwnerCard = () => {
+interface OwnerCardProps extends HTMLAttributes<HTMLDivElement> {
+  githubUser?: GithubUser;
+}
+
+export const OwnerCard = ({githubUser, ...props}: OwnerCardProps) => {
   return (
-    <Card>
-      <CardHeader>
-        <div className={'flex gap-5'}>
-          <Avatar className={'h-60 w-60'}>
-            <AvatarImage src="https://github.com/DavidApril.png" alt="@DavidApril"/>
-            <AvatarFallback>D</AvatarFallback>
+    <Card {...props}>
+      {githubUser
+        ? <CardHeader className={'flex flex-row gap-3'}>
+          <Avatar className={'h-20 w-20'}>
+            <AvatarImage src={githubUser.avatar_url} alt="@DavidApril"/>
+            <AvatarFallback>JD</AvatarFallback>
           </Avatar>
-          <div className={'flex flex-col gap-2'}>
-            <CardTitle>Jesús David Agámez Cantillo</CardTitle>
-            <CardDescription>Front End Developer</CardDescription>
-            <CardDescription className={'text-xl'}>
-              Software Engineering student with more than a year and a half of experience as a front-end developer. My
-              ability to face new challenges and learn in an agile way allows me to successfully integrate into new
-              projects, quickly adapting to a new team and its methodology.
-            </CardDescription>
+          <div>
+            <CardTitle>{githubUser.name}</CardTitle>
+            <CardDescription>Software Engineer Student</CardDescription>
+            <CardDescription className={'text-xl my-3'}>{githubUser.bio}</CardDescription>
             <CardDescription className={'flex flex-wrap gap-1'}>
               <SocialNetworkBadgeWithTooltip
-                tooltipContent={'Go to Github'} label={'@DavidApril'}
+                tooltipContent={`Go to ${githubUser.login} profile`}
+                label={`Github - ${githubUser.followers} followers`}
                 to={'https://github.com/DavidApril'}/>
               <SocialNetworkBadgeWithTooltip
                 tooltipContent={'Go to linkedin'} label={'LinkedIn'}
                 to={'https://www.linkedin.com/in/david-ag%C3%A1mez/'}/>
+              <SocialNetworkBadgeWithTooltip
+                tooltipContent={`Send email`} label={`${githubUser.email}`}
+                to={'https://github.com/DavidApril'}/>
             </CardDescription>
           </div>
-        </div>
-      </CardHeader>
+        </CardHeader>
+        : <AvatarSkeleton/>}
       <CardContent className={'flex'}>
-        <ChartPie/>
-        <ChartPie/>
-        <ChartPie/>
+        {/*<GithubStats/>*/}
       </CardContent>
     </Card>
   );
